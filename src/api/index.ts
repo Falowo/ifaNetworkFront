44f4433f861bfd0e3f3ee9da272e9c8c10f350a0
josselin <obarayego@gmail.com>
2023-02-24 23:02:45 +0100

    let's try with a proper callback

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,8 +2,6 @@ import axios from "axios";
 
 export const url = process.env.REACT_APP_API_URL;
 
-
-
 // export const instance = () =>
 //   axios.create({
 //     baseURL: url,
@@ -17,14 +15,18 @@ export const url = process.env.REACT_APP_API_URL;
 //   baseURL: url,
 //   timeout: 60000,
 // });
-export const instance = (token:string) =>
+export const instance = (token: string) =>
   axios.create({
     baseURL: url,
-    timeout: 60000,
     headers: {
-      Authorization: `Bearer ${token}`,
+      "content-type": "application/json",
+      authorization: `Bearer ${token}`,
+      "cache-control": "no-cache",
     },
+    data: { grant_types: "authorization_code" },
   });
+
+
 
 // export const authInstance = axios.create({
 //   baseURL: url,
