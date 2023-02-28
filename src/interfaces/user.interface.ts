4a4d5e738b677334aa6fe9197393883832a35e01
josselin <obarayego@gmail.com>
2023-02-28 16:32:46 +0100

    userModel && interface23

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -6,27 +6,27 @@ export enum Status {
 
 export interface IUser {
   _id?: string;
-  sub?: String;
+  sub?: string;
   createdAt?: Date;
   updatedAt?: Date;
-  name?: String;
-  given_name?: String;
-  family_name?: String;
-  middle_name?: String;
-  nickname?: String;
-  preferred_username?: String;
-  profile?: String;
-  picture?: String;
-  website?: String;
-  email?: String;
+  name?: string;
+  given_name?: string;
+  family_name?: string;
+  middle_name?: string;
+  nickname?: string;
+  preferred_username?: string;
+  profile?: string;
+  picture?: string;
+  website?: string;
+  email?: string;
   email_verified?: boolean;
-  gender?: String;
-  birthdate?: String;
-  zoneinfo?: String;
-  locale?: String;
-  phone_number?: String;
+  gender?: string;
+  birthdate?: string;
+  zoneinfo?: string;
+  locale?: string;
+  phone_number?: string;
   phone_number_verified?: boolean;
-  address?: String;
+  address?: string;
   updated_at?: string;
   friendsIds?: string[];
   followersIds?: string[];
@@ -34,7 +34,7 @@ export interface IUser {
   blockedIds?: string[];
   isAdmin?: boolean;
   isBabalawo?: boolean;
-  desc?: String;
+  desc?: string;
   status?: Status;
   [key: string]: any;
 }
@@ -47,20 +47,3 @@ export interface ToUpdateUserDesc {
   desc?: string;
 }
 
-// export interface IFakeUser {
-//   token?: string;
-//   _id?: string;
-//   username: string;
-//   email?: string;
-//   password?: string;
-//   profilePicture?: string;
-//   coverPicture?: string;
-//   followersIds?: string[];
-//   followedIds?: string[];
-//   isAdmin?: boolean;
-//   desc?: string;
-//   city?: string;
-//   from?: string;
-//   relationship?: Relationship;
-//   birthDate?: Date;
-// }
