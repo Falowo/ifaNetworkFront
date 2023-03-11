7ec8d49550d346b12eb580ab555e1be1a4ae37ca
josselin <obarayego@gmail.com>
2023-03-11 22:22:59 +0100

    tinymce2 unused var

--- src/components/tinymceEditor/tinymceEditor.tsx
+++ src/components/tinymceEditor/tinymceEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 
 export default function TinymceEditor() {
